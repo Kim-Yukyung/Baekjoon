7ef85db949f158203dc5d31b2b3d2dc17d10ffcc
Kim-Yukyung <154517645+Kim-Yukyung@users.noreply.github.com>
2024-02-01 19:50:50 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 740 ms, Memory: 141784 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,55 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean [] visited;
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String [] temp = bf.readLine().split(" ");
-        int V = Integer.parseInt(temp[0]);
-        int E = Integer.parseInt(temp[1]);
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        ArrayList<Integer>[] graph = new ArrayList[V];
+        graph = new ArrayList[V];
         for (int i = 0; i < V; i++) {
             graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < E; i++) {
-            temp = bf.readLine().split(" ");
-            int x = Integer.parseInt(temp[0]);
-            int y = Integer.parseInt(temp[1]);
+            st = new StringTokenizer(bf.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
 
-            graph[x - 1].add(y);
-            graph[y - 1].add(x);
+            graph[x].add(y);
+            graph[y].add(x);
         }
 
-        int index = 0;
-        boolean [] visited = new boolean[V];
-
-        boolean flag = true;
         int cnt = 0;
+        visited = new boolean[V];
 
         for (int i = 0; i < visited.length; i++) {
-            if (visited[i] != true) {
-                index = i;
-                flag = false;
-                break;
-            }
-        }
-
-        while(flag != true) {
-            for (int i = 0; i < visited.length; i++) {
-                if (visited[i] != true) {
-                    index = i;
-                    flag = false;
-                    break;
-                }
-                else flag = true;
-            }
-
-            if(flag == false) {
-                DFS(V, index, graph, visited);
+            if (!visited[i]) {
+                DFS(i);
                 cnt++;
             }
         }
@@ -57,12 +41,12 @@ public class Main {
         System.out.print(cnt);
     }
 
-    static void DFS(int V, int index, ArrayList<Integer>[] graph, boolean [] visited){
+    static void DFS(int index){
         visited[index] = true;
 
         for(int node: graph[index]){
-            if(!visited[node-1])
-                DFS(V, node-1, graph, visited);
+            if(!visited[node])
+                DFS(node);
         }
     }
 }
