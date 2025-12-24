66290e31b4ef0b9f0debe75f7f622aa692341a6a
Kim-Yukyung <154517645+Kim-Yukyung@users.noreply.github.com>
2025-12-25 05:19:15 +0900

    [Bronze II] Title: 콘테스트, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5576. 콘테스트/콘테스트.py
@@ -0,0 +1,8 @@
+import sys
+
+input = sys.stdin.readline
+
+w = sorted([int(input()) for _ in range(10)], reverse=True)[:3]
+k = sorted([int(input()) for _ in range(10)], reverse=True)[:3]
+
+print(sum(w), sum(k))
