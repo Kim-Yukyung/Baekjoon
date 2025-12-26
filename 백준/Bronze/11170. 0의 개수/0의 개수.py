c7b8afac3f0761e752fec2a35af975c684bc4a4f
Kim-Yukyung <154517645+Kim-Yukyung@users.noreply.github.com>
2025-12-26 15:51:05 +0900

    [Bronze I] Title: 0의 개수, Time: 1860 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11170. 0의 개수/0의 개수.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    cnt = 0
+    for i in range(a, b+1):
+        cnt += str(i).count('0')
+    print(cnt)
